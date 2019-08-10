71b5aac5f51eea47e5f10842b5ea8a5182001c84
Vitaly Khabarov <vitkhab@users.noreply.github.com>
2019-08-10 10:50:53 +0300

    Update lambda function in basic example
    + Add logger
    + Add more information to logging
    + Add docstrings
    * Fix pylint warnings

--- examples/basic/lambda/main.py
+++ examples/basic/lambda/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
-# Hack to use dependencies from lib directory
-import sys
+'''
+Echo service as a Lambda function
+'''
+
 import os
-base_path = os.path.dirname(__file__)
-sys.path.append(base_path + "/lib")
-
+import sys
 import json
+import logging
+from pprint import pformat
 
-def response(status=200, headers={'Content-Type': 'application/json'}, body=''):
+# Hack to use dependencies from lib directory
+BASE_PATH = os.path.dirname(__file__)
+sys.path.append(BASE_PATH + "/lib")
+
+LOGGER = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.INFO)
+
+def response(status=200, headers=None, body=''):
     '''
     http://www.awslessons.com/2017/lambda-api-gateway-internal-server-error/
     '''
     if not body:
-        return { 'statusCode': status }
+        return {'statusCode': status}
+
+    if headers is None:
+        headers = {'Content-Type': 'application/json'}
+
     return {
         'statusCode': status,
         'headers': headers,
@@ -20,9 +33,14 @@ def response(status=200, headers={'Content-Type': 'application/json'}, body=''):
     }
 
 def lambda_handler(event, context):
-    print(event)
+    '''
+    This function is called on HTTP request.
+    It logs the request and an execution context. Then it returns body of the request.
+    '''
+    LOGGER.info("%s", pformat({"Context" : vars(context), "Request": event}))
     return response(status=200, body=event['body'])
 
 
 if __name__ == '__main__':
+    # Do nothing if executed as a script
     pass
